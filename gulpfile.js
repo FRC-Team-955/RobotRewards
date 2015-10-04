425048c79489ea0f3ea3838a72ed83cb40e9c056
Graham Barber <grahammbarber@gmail.com>
2015-10-03 22:30:24 -0700

    Split gulp build into parts

--- gulpfile.js
+++ gulpfile.js
@@ -5,29 +5,35 @@ var gulp    = require("gulp"),
 	react   = require("gulp-react"),
 	uglify  = require("gulp-uglify");
 
-gulp.task("build", function () {
+gulp.task("build:jade", function () {
 	gulp.src("src/public/index.jade")
 		.pipe(jade())
 		.pipe(minHTML())
 		.pipe(gulp.dest("dist/public/"));
+});
 
+gulp.task("build:jsx", function () {
 	gulp.src("src/public/jsx/**.jsx")
 		.pipe(react())
 		.pipe(uglify({ mangle: false }))
 		.pipe(gulp.dest("dist/public/js/"));
+});
 
+gulp.task("build:css", function () {
 	gulp.src("src/public/css/**.css")
 		.pipe(minCSS())
 		.pipe(gulp.dest("dist/public/css/"));
+});
 
+gulp.task("build:serv", function () {
 	gulp.src("src/app.js")
 		.pipe(uglify({ mangle: false }))
 		.pipe(gulp.dest("dist/"));
 });
 
 gulp.task("watch", function () {
-	gulp.watch("src/public/index.jade", ["build"]);
-	gulp.watch("src/public/jsx/**.jsx", ["build"]);
-	gulp.watch("src/public/css/**.css", ["build"]);
-	gulp.watch("src/app.js", ["build"]);
+	gulp.watch("src/public/index.jade", ["build:jade"]);
+	gulp.watch("src/public/jsx/**.jsx", ["build:jsx"]);
+	gulp.watch("src/public/css/**.css", ["build:css"]);
+	gulp.watch("src/app.js", ["build:serv"]);
 });
