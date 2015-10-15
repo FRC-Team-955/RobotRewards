32423177144502d9d4840bf1423f94f3a939ed56
Graham Barber <grahammbarber@gmail.com>
2015-10-15 10:28:11 -0700

    Changed config.js to example, added to gitignore

---
+++ src/config.example.js
@@ -0,0 +1,15 @@
+module.exports = {
+	exp: {
+		port: 3000
+	},
+	rdb: {
+		host: "example.com",
+		port: 28015,
+		authkey: "",
+		db: "robot-rewards"
+	},
+	ds: {
+		host: "example.com",
+		port: 6020
+	}
+};
