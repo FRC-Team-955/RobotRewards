da19fbfd8416f4db356c5443533447e84de73cc7
Graham Barber <grahammbarber@gmail.com>
2015-10-15 18:54:49 -0700

    Added basic server Express and Deepstream code

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+/* global __dirname */
 var express            = require("express"),        // Node webserver framework.
 	r                  = require("rethinkdb"),      // Database connector.
 	morgan             = require("morgan"),         // Request logger.
@@ -8,4 +9,21 @@ var express            = require("express"),        // Node webserver framework.
 var app = express(),                   // Init webserver object
 	server = new DSServer();           // Init realtime server object
 
-var config = require("config.js");     // Load configuration object
+var config = require( __dirname + "/config.js");     // Load configuration object
+
+// Express configuration
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
+app.listen(config.exp.port);
+console.log("Express is ready to go on port " + config.exp.port);
+
+// RethinkDB configuration
+// r.connect(config.rdb);
+
+
+// Deepstream configuration
+server.set("host", config.ds.host);
+server.set("port", config.ds.port);
+
+server.start();
--- src/config.js
+++ src/config.js
@@ -1,11 +1,15 @@
 module.exports = {
 	exp: {
-
+		port: 3000
 	},
 	rdb: {
-
+		host: "localhost",
+		port: 28015,
+		authkey: "",
+		db: "robot-rewards"
 	},
 	ds: {
-
+		host: "localhost",
+		port: 6020
 	}
 };
