7608390f9dc4ff03627740106dcf327c90771a7c
Graham Barber <grahammbarber@gmail.com>
2015-10-23 08:11:52 -0700

    Trimming, commenting server

--- src/app.js
+++ src/app.js
@@ -1,25 +1,21 @@
 /* global __dirname */
-var express            = require("express"),        // Node webserver framework.
-	morgan             = require("morgan"),         // Request logger.
-	bodyParser         = require("body-parser"),    // Convert requests to REST standards.
-	DSServer           = require("deepstream.io"),  // Realtime server framework.
-	DSStorageConnector = require("deepstream.io-storage-rethinkdb");  // Database connector.
+var express            = require("express"),               // Node webserver framework.
+	morgan             = require("morgan"),                // Request logger.
+	DSServer           = require("deepstream.io"),         // Realtime server framework.
+	DSStorageConnector = require("deepstream.io-storage-rethinkdb"); // Database connector.
 
-var app = express(),                   // Init webserver object
-	server = new DSServer();           // Init realtime server object
+var app = express(),                   // Init webserver object.
+	server = new DSServer();           // Init realtime server object.
 
-var config = require( __dirname + "/config.js");     // Load configuration object
+var config = require( __dirname + "/config.js");           // Load config object.
 
-// Express configuration
-app.use(bodyParser.json());
-app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
-
-app.listen(config.exp.port);
-console.log("Express is ready to go on port " + config.exp.port);
+// Express configuration;
+app.listen(config.exp.port);           // Launch web server.
+console.log("Express is ready to go on port " + config.exp.port);    // Notify user.
 
 // Deepstream configuration
-server.set("host", config.ds.host);
-server.set("port", config.ds.port);
-server.set("storage", new DSStorageConnector(config.rdb));
+server.set("host", config.ds.host);    // Set hostname.
+server.set("port", config.ds.port);    // Set port.
+server.set("storage", new DSStorageConnector(config.rdb));           // Configure database connector.
 
-server.start();
+server.start();    // Launch deepstream
